5658d2f185d7ba15de2eaf24cb0930e80547a535
mdlawrenceusa <mdlawrence@me.com>
2023-09-07 14:54:32 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { ProductListComponent } from './product-list/product-list.component';
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: ProductListComponent },ProductListComponent
     ])
   ],
   declarations: [
